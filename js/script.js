baa51712ccfc851e6553259672634f3fd76531c0
davidzeck <edsodavid07@gmail.com>
2020-02-16 20:21:41 +0200

    finalised-project

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 
 function submt(){
-     var gender = document.getElementById("gender").checked;
-     var date = document.getElementById("date");
-     //alert(gender);
-    
+    //Get the variables from the form 
+    var gender = document.getElementById("gender").checked;
+     var date = document.getElementById("date").value;
+     var akan = document.getElementById("akan")
+
+     //get day of week using JS object
+     var dt = new Date(date);
+     var selectedDay = dt.getDay();
+     
      if(gender == true){
          //male
-         var maleName=("Kwasi" , "Kwadwo" , "Kwabena" ,"Kwaku" , "Yaw" , "Kofi" , "Kwame")
-         alert(maleName).
-     }else if(gender == false){
+         var maleName=["Kwasi" , "Kwadwo" , "Kwabena" ,"Kwaku" , "Yaw" , "Kofi" , "Kwame"];
+         akan.value = maleName[selectedDay];
+     }else{
          //female
-         var femaleName=("Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , " Afua" , "Ama" )
-         alert(femaleName);
+         var femaleName=["Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , " Afua" , "Ama" ];
+         akan.value = femaleName[selectedDay];
      }
-     var dayofweek =("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
-     
+
 }
